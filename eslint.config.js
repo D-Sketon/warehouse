52581851d7fd0b8ad97b76091cbbdc63eccc5525
D-Sketon <2055272094@qq.com>
2025-04-22 22:53:42 +0800

    chore: fix test eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 const config = require('eslint-config-hexo/ts');
+const testConfig = require('eslint-config-hexo/test');
 
 module.exports = [
+  // Configurations applied globally
   ...config,
   {
     'rules': {
@@ -14,6 +16,27 @@ module.exports = [
       '@typescript-eslint/no-unused-vars': [
         'error', {
           'argsIgnorePattern': '^_'
+        }
+      ]
+    }
+  },
+  // Configurations applied only to test files
+  {
+    files: [
+      'test/**/*.ts'
+    ],
+    languageOptions: {
+      ...testConfig.languageOptions
+    },
+    rules: {
+      ...testConfig.rules,
+      '@typescript-eslint/ban-ts-comment': 0,
+      '@typescript-eslint/no-unused-expressions': 0,
+      '@typescript-eslint/no-unused-vars': [
+        'error', {
+          'varsIgnorePattern': '^_',
+          'argsIgnorePattern': '^_',
+          'caughtErrorsIgnorePattern': '^_'
         }
       ]
     }
